f2e0599450420f70941520b534050224c1f5c4b9
lorenzomammana <lorenzom96@hotmail.it>
2019-11-02 18:25:30 +0100

    Init repo

---
+++ scraper.py
@@ -0,0 +1,126 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import re
+from requests.adapters import ProxyError
+
+
+proxies = open("proxies.txt", "r").readlines()
+
+
+def change_proxy(count):
+    return {"http": proxies[count].split("\n")[0]}
+
+
+proxy = {"http": "157.230.112.218:8080"}
+count_proxy = 0
+url = 'http://www.darklyrics.com/'
+
+while True:
+    try:
+        response = requests.get(url + 'a.html', proxies=proxy)
+    except ProxyError:
+        proxy = change_proxy(count_proxy)
+        count_proxy += 1
+        continue
+    break
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+all_artists_div = soup.find("div", {"class": "cont"})
+all_artists = all_artists_div.findAll('a')
+
+output = open("darklyrics.csv", "w+", encoding="utf-8")
+
+restart = True
+
+for artist in all_artists:
+    link = artist['href']
+    band_name = artist.text
+
+    if band_name == "AFTERBLOOD":
+        restart = False
+
+    if restart:
+        continue
+
+    temp_out = open("temp.csv", "a", encoding="utf-8")
+    print(band_name)
+    time.sleep(2)
+
+    while True:
+        try:
+            albums = requests.get(url + link, proxies=proxy)
+        except ProxyError:
+            proxy = change_proxy(count_proxy)
+            count_proxy += 1
+            continue
+        break
+
+    albums = BeautifulSoup(albums.text, "html.parser")
+    all_albums_div = albums.findAll("div", {"class": "album"})
+
+    for album in all_albums_div:
+        all_albums = album.findAll('a')
+
+        try:
+            link = all_albums[0]['href']
+        except IndexError:
+            continue
+
+        try:
+            album_name = album.find("h2").text.split('"')[1].replace('"', "")
+            year = album.find("h2").text.split('"')[2].replace(' (', "").replace(")", "")
+        except IndexError:
+            continue
+
+        print(album_name, year)
+        time.sleep(2)
+
+        while True:
+            try:
+                songs = requests.get(url + link, proxies=proxy)
+                songs = BeautifulSoup(songs.text, "html.parser")
+
+                if songs.find("div", {"class": "note"}) is not None:
+                    songs.find("div", {"class": "note"}).decompose()
+
+                if songs.find("div", {"class": "thanks"}) is not None:
+                    songs.find("div", {"class": "thanks"}).decompose()
+
+                all_lyrics = songs.find("div", {"class": "lyrics"})
+                all_lyrics.findAll("a")[-1].decompose()
+            except (ProxyError, AttributeError):
+                proxy = change_proxy(count_proxy)
+                print(proxy)
+                count_proxy += 1
+                continue
+            break
+
+        all_lyrics = all_lyrics.text.replace(",", "")
+        all_lyrics = all_lyrics.replace('"', '')
+        song_lyrics = ""
+        title = ""
+
+        for lyric in all_lyrics.split("\n"):
+
+            if lyric == '':
+                continue
+
+            if re.match("[0-9]*\.", lyric) is not None:
+                if title != "":
+                    output.write(band_name + ',' + album_name + ',' + year + ',' + title + ',"' + song_lyrics + '"\n')
+                    temp_out.write(band_name + ',' + album_name + ',' + year + ',' + title + ',"' + song_lyrics + '"\n')
+
+                title = lyric.split(".")[1].strip()
+                song_lyrics = ""
+            else:
+                song_lyrics += lyric + " "
+
+        output.write(band_name + ',' + album_name + ',' + year + ',' + title + ',"' + song_lyrics + '"\n')
+        temp_out.write(band_name + ',' + album_name + ',' + year + ',' + title + ',"' + song_lyrics + '"\n')
+        time.sleep(2)
+
+    temp_out.close()
+output.close()
