2139fd38a2bcfa0a83cbc8475a6e09adcd8ec171
lorenzomammana <lorenzom96@hotmail.it>
2020-01-14 17:15:35 +0100

    Create singol label mlp

--- single-label/train-mlp.py
+++ single-label/train-mlp.py
@@ -1,12 +1,16 @@
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import classification_report
+import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from keras import Sequential, Input
-from keras import backend as K
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras import backend as K
+import os
+os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
 
 def recall_m(y_true, y_pred):
@@ -30,39 +34,44 @@ def f1_m(y_true, y_pred):
 
 
 if __name__ == '__main__':
-    darklyrics = pd.read_csv('darklyrics-proc-tokens.csv',
-                             converters={'tokens': lambda x: x.strip("[]").replace("'", "").split(", "),
-                                         'genre': lambda x: x.strip("[]").replace("'", "").split(", ")})
+    darklyrics = pd.read_csv('darklyrics-proc-tokens-single.csv',
+                             converters={'tokens': lambda x: x.strip("[]").replace("'", "").split(", ")})
 
     corpus = darklyrics.apply(lambda x: " ".join(x['tokens']), axis=1)
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(corpus)
-
-    labels = darklyrics.apply(lambda x: set(x['genre']), axis=1)
+    X = X.todense()
+    X = np.array(X)
+    labels = darklyrics['genre']
 
     X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=0)
 
-    mlb = MultiLabelBinarizer(classes=['Heavy Metal', 'Thrash Metal', 'Power Metal',
-                                       'Folk Metal', 'Progressive Metal', 'Death Metal',
-                                       'Doom Metal', 'Black Metal', 'Rock'])
-    y_train = [each for each in y_train]
-    y_train = mlb.fit_transform(y_train)
-    y_test = [each for each in y_test]
-    y_test = mlb.fit_transform(y_test)
+    encoder = LabelEncoder()
+    y_train = encoder.fit_transform(y_train)
+    y_test = encoder.transform(y_test)
+
+    onehot = OneHotEncoder(sparse=False)
+    y_train = onehot.fit_transform(y_train.reshape(len(y_train), 1))
+    y_test_oh = onehot.fit_transform(y_test.reshape(len(y_test), 1))
 
     model = Sequential()
     model.add(Dense(256, activation='relu', input_shape=(1305,)))
     model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(9, activation='sigmoid'))
+    model.add(Dense(9, activation='softmax'))
 
     mcp_save = ModelCheckpoint('best_mlp.h5', save_best_only=True, monitor='val_f1_m', mode='max')
+    reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
 
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc', f1_m])
+    opt = Adam(lr=0.01)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['acc', f1_m])
 
-    model.fit(X_train, y_train, batch_size=64, epochs=30, verbose=1,
-              validation_data=(X_test, y_test), callbacks=[mcp_save])
+    model.fit(X_train, y_train, batch_size=64, epochs=50, verbose=2,
+              validation_data=(X_test, y_test_oh), callbacks=[mcp_save, reduce_lr_loss])
 
     model.load_weights('best_mlp.h5')
 
-    model.evaluate(X_test, y_test)
+    preds = model.predict(X_test)
+    preds = np.argmax(preds, axis=1)
+
+    print(classification_report(y_test, preds))
